5183fbe7996d6190ba57c6a539975da2c2b4a2d1
Matt Aronoff <matthew.aronoff@nist.gov>
2012-03-20 16:16:51 -0400

    Device link refactoring partially complete

---
+++ wsabi2/NBCLDeviceLinkManager.h
@@ -0,0 +1,13 @@
+//
+//  NBCLDeviceLinkManager.h
+//  wsabi2
+//
+//  Created by Matt Aronoff on 3/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NBCLDeviceLinkManager : NSObject
+
+@end
---
+++ wsabi2/NBCLDeviceLinkManager.m
@@ -0,0 +1,13 @@
+//
+//  NBCLDeviceLinkManager.m
+//  wsabi2
+//
+//  Created by Matt Aronoff on 3/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NBCLDeviceLinkManager.h"
+
+@implementation NBCLDeviceLinkManager
+
+@end
