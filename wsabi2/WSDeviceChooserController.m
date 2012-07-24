NIST-BWS/wsabi2

commit 8cae815f5d04e45bda300ada505ae8e62b05e142

Fixes #68.
* Add a TapGestureRecognizer to the containing window to detect only taps that are no on the frontmost view.  If detected, dismiss the modal dialog.
* Remove existing cancel buttons.

wsabi2/WSDeviceChooserController.m
@@ -16,6 +16,8 @@
 @synthesize item;
 @synthesize autodiscoveryEnabled;
 @synthesize currentButton;
+@synthesize tapBehindViewRecognizer;
+
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -41,21 +43,6 @@
     [super viewDidLoad];
 
     self.title = [WSModalityMap stringForCaptureType:self.submodality];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-
-    //If this is the root view controller, we're launching the config workflow partway through.
-    //We need a way to leave this controller in that case.
-    if (self == [self.navigationController.viewControllers objectAtIndex:0]) {
-        UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonPressed:)];
-        
-        self.navigationItem.leftBarButtonItem = cancelButton;
-    }
-    
 
     //Fetch a list of recent sensors from Core Data
     
@@ -120,9 +107,9 @@
 
 - (void)viewDidUnload
 {
+    [self setTapBehindViewRecognizer:nil];
+    
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -133,10 +120,18 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
+    // Add recognizer to detect taps outside of the modal view
+    [[self tapBehindViewRecognizer] setCancelsTouchesInView:NO];
+    [[self tapBehindViewRecognizer] setNumberOfTapsRequired:1];
+    [[[self view] window] addGestureRecognizer:[self tapBehindViewRecognizer]];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+    // Remove recognizer when view isn't visible
+    [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+    
     [super viewWillDisappear:animated];
 }
 
@@ -151,18 +146,31 @@
 	return YES;
 }
 
-#pragma mark - Button action methods
--(IBAction) cancelButtonPressed:(id)sender
+- (IBAction)tappedBehindView:(id)sender
 {
-    //post a notification to hide the device chooser and return to the previous state
-    NSDictionary* userInfo = [NSDictionary dictionaryWithObject:item forKey:kDictKeyTargetItem];
-    [[NSNotificationCenter defaultCenter] postNotificationName:kCancelWalkthroughNotification
-                                                        object:self
-                                                      userInfo:userInfo];
-
-    [self dismissModalViewControllerAnimated:YES];
+    UITapGestureRecognizer *recognizer = (UITapGestureRecognizer *)sender;
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        // Get coordinates in the window of tap
+        CGPoint location = [recognizer locationInView:nil];
+        
+        // Check if tap was within view
+        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+            [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+            
+            //post a notification to hide the device chooser and return to the previous state
+            NSDictionary* userInfo = [NSDictionary dictionaryWithObject:item forKey:kDictKeyTargetItem];
+            [[NSNotificationCenter defaultCenter] postNotificationName:kCancelWalkthroughNotification
+                                                                object:self
+                                                              userInfo:userInfo];
+            
+            [self dismissModalViewControllerAnimated:YES];
+        }
+    }
 }
 
+#pragma mark - Button action methods
 -(IBAction) currentButtonPressed:(id)sender
 {
     //Push a new controller to configure the device.

wsabi2/WSDeviceSetupController.m
@@ -20,6 +20,7 @@
 @synthesize deviceDefinition;
 @synthesize modality;
 @synthesize submodality;
+@synthesize tapBehindViewRecognizer;
 
 //Status stuff
 @synthesize sensorCheckStatus;
@@ -123,8 +124,20 @@
 
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    // Add recognizer to detect taps outside of the modal view
+    [[self tapBehindViewRecognizer] setCancelsTouchesInView:NO];
+    [[self tapBehindViewRecognizer] setNumberOfTapsRequired:1];
+    [[[self view] window] addGestureRecognizer:[self tapBehindViewRecognizer]];
+}
+
 -(void) viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
+    
     //work around a bug in iOS – adding a white activity indicator in Interface Builder, the indicator doesn't stay
     //white.
     self.checkingActivity.color = [UIColor whiteColor];
@@ -139,18 +152,40 @@
     //whatever the reason for disappearing, cancel all of our network operations
     [currentLink cancelAllOperations];    
     
+    // Remove recognizer when view isn't visible
+    [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+    
+    [super viewWillDisappear:animated];
 }
 
 - (void)viewDidUnload
 {
+    [self setTapBehindViewRecognizer:nil];
+    
     [super viewDidUnload];
-
- }
+}
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
 	return YES;
+}
+
+- (IBAction)tappedBehindView:(id)sender
+{
+    UITapGestureRecognizer *recognizer = (UITapGestureRecognizer *)sender;
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        // Get coordinates in the window of tap
+        CGPoint location = [recognizer locationInView:nil];
+        
+        // Check if tap was within view
+        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+            [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+            [self dismissModalViewControllerAnimated:YES];
+        }
+    }
 }
 
 #pragma mark - Property Getters/Setters

wsabi2/WSModalityChooserController.m
@@ -11,6 +11,7 @@
 @implementation WSModalityChooserController
 @synthesize item;
 @synthesize currentButton;
+@synthesize tapBehindViewRecognizer;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -37,16 +38,6 @@
     
     self.title = @"Capture type";
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
-    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancelButtonPressed:)];
-    
-    self.navigationItem.leftBarButtonItem = cancelButton;
-    
     if (self.item.managedObjectContext && self.item.modality) {
         self.currentButton = [[UIBarButtonItem alloc] initWithTitle:[NSString stringWithFormat:@"Keep \"%@\"",self.item.modality]
                                                               style:UIBarButtonItemStyleDone
@@ -57,9 +48,9 @@
 
 - (void)viewDidUnload
 {
+    [self setTapBehindViewRecognizer:nil];
+    
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -70,10 +61,18 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
+    // Add recognizer to detect taps outside of the modal view
+    [[self tapBehindViewRecognizer] setCancelsTouchesInView:NO];
+    [[self tapBehindViewRecognizer] setNumberOfTapsRequired:1];
+    [[[self view] window] addGestureRecognizer:[self tapBehindViewRecognizer]];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+    // Remove recognizer when view isn't visible
+    [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+    
     [super viewWillDisappear:animated];
 }
 
@@ -88,17 +87,30 @@
 	return YES;
 }
 
-#pragma mark - Button action methods
--(IBAction) cancelButtonPressed:(id)sender
+- (IBAction)tappedBehindView:(id)sender
 {
-    [self dismissModalViewControllerAnimated:YES];
-    //post a notification to hide the device chooser and return to the previous state
-    NSDictionary* userInfo = [NSDictionary dictionaryWithObject:item forKey:kDictKeyTargetItem];
-    [[NSNotificationCenter defaultCenter] postNotificationName:kCancelWalkthroughNotification
-                                                        object:self
-                                                      userInfo:userInfo];
+    UITapGestureRecognizer *recognizer = (UITapGestureRecognizer *)sender;
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        // Get coordinates in the window of tap
+        CGPoint location = [recognizer locationInView:nil];
+        
+        // Check if tap was within view
+        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+            [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+            
+            //post a notification to hide the device chooser and return to the previous state
+            NSDictionary* userInfo = [NSDictionary dictionaryWithObject:item forKey:kDictKeyTargetItem];
+            [[NSNotificationCenter defaultCenter] postNotificationName:kCancelWalkthroughNotification
+                                                                object:self
+                                                              userInfo:userInfo];
+            [self dismissModalViewControllerAnimated:YES];
+        }
+    }
 }
 
+#pragma mark - Button action methods
 -(IBAction) currentButtonPressed:(id)sender
 {
     //Push a new controller to choose the submodality.

wsabi2/WSSubmodalityChooserController.m
@@ -12,6 +12,7 @@
 @synthesize modality;
 @synthesize item;
 @synthesize currentButton;
+@synthesize tapBehindViewRecognizer;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -60,9 +61,9 @@
 
 - (void)viewDidUnload
 {
+    [self setTapBehindViewRecognizer:nil];
+    
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -73,10 +74,18 @@
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    
+    // Add recognizer to detect taps outside of the modal view
+    [[self tapBehindViewRecognizer] setCancelsTouchesInView:NO];
+    [[self tapBehindViewRecognizer] setNumberOfTapsRequired:1];
+    [[[self view] window] addGestureRecognizer:[self tapBehindViewRecognizer]];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+    // Remove recognizer when view isn't visible
+    [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+    
     [super viewWillDisappear:animated];
 }
 
@@ -110,6 +119,22 @@
     
 }
 
+- (IBAction)tappedBehindView:(id)sender
+{
+    UITapGestureRecognizer *recognizer = (UITapGestureRecognizer *)sender;
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        // Get coordinates in the window of tap
+        CGPoint location = [recognizer locationInView:nil];
+        
+        // Check if tap was within view
+        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+            [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
+            [self dismissModalViewControllerAnimated:YES];
+        }
+    }
+}
 
 #pragma mark - Table view data source
 

wsabi2/WSDeviceChooserController.h
@@ -20,7 +20,7 @@
     NSMutableArray *recentSensors;
 }
 
--(IBAction) cancelButtonPressed:(id)sender;
+-(IBAction) tappedBehindView:(id)sender;
 -(IBAction) currentButtonPressed:(id)sender;
 
 @property (nonatomic) BOOL autodiscoveryEnabled;
@@ -29,5 +29,6 @@
 
 @property (nonatomic, strong) WSCDItem *item;
 @property (nonatomic, strong) UIBarButtonItem *currentButton;
+@property (nonatomic, strong) IBOutlet UITapGestureRecognizer *tapBehindViewRecognizer;
 
 @end

wsabi2/WSDeviceChooserController.xib
@@ -1,17 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <archive type="com.apple.InterfaceBuilder3.CocoaTouch.iPad.XIB" version="8.00">
 	<data>
-		<int key="IBDocument.SystemTarget">1280</int>
-		<string key="IBDocument.SystemVersion">11D50</string>
-		<string key="IBDocument.InterfaceBuilderVersion">1938</string>
-		<string key="IBDocument.AppKitVersion">1138.32</string>
-		<string key="IBDocument.HIToolboxVersion">568.00</string>
+		<int key="IBDocument.SystemTarget">1296</int>
+		<string key="IBDocument.SystemVersion">11E53</string>
+		<string key="IBDocument.InterfaceBuilderVersion">2182</string>
+		<string key="IBDocument.AppKitVersion">1138.47</string>
+		<string key="IBDocument.HIToolboxVersion">569.00</string>
 		<object class="NSMutableDictionary" key="IBDocument.PluginVersions">
 			<string key="NS.key.0">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
-			<string key="NS.object.0">933</string>
+			<string key="NS.object.0">1181</string>
 		</object>
 		<array key="IBDocument.IntegratedClassDependencies">
 			<string>IBProxyObject</string>
+			<string>IBUITapGestureRecognizer</string>
 			<string>IBUITableView</string>
 		</array>
 		<array key="IBDocument.PluginDependencies">
@@ -36,7 +37,6 @@
 				<string key="NSFrameSize">{540, 576}</string>
 				<reference key="NSSuperview"/>
 				<reference key="NSWindow"/>
-				<reference key="NSNextKeyView"/>
 				<object class="NSColor" key="IBUIBackgroundColor">
 					<int key="NSColorSpace">1</int>
 					<bytes key="NSRGB">MCAwIDAgMAA</bytes>
@@ -54,6 +54,7 @@
 				<float key="IBUISectionHeaderHeight">10</float>
 				<float key="IBUISectionFooterHeight">10</float>
 			</object>
+			<object class="IBUITapGestureRecognizer" id="61197634"/>
 		</array>
 		<object class="IBObjectContainer" key="IBDocument.Objects">
 			<array class="NSMutableArray" key="connectionRecords">
@@ -64,6 +65,14 @@
 						<reference key="destination" ref="993928743"/>
 					</object>
 					<int key="connectionID">11</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBCocoaTouchOutletConnection" key="connection">
+						<string key="label">tapBehindViewRecognizer</string>
+						<reference key="source" ref="372490531"/>
+						<reference key="destination" ref="61197634"/>
+					</object>
+					<int key="connectionID">13</int>
 				</object>
 				<object class="IBConnectionRecord">
 					<object class="IBCocoaTouchOutletConnection" key="connection">
@@ -80,6 +89,14 @@
 						<reference key="destination" ref="372490531"/>
 					</object>
 					<int key="connectionID">10</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBCocoaTouchEventConnection" key="connection">
+						<string key="label">tappedBehindView:</string>
+						<reference key="source" ref="61197634"/>
+						<reference key="destination" ref="372490531"/>
+					</object>
+					<int key="connectionID">14</int>
 				</object>
 			</array>
 			<object class="IBMutableOrderedSet" key="objectRecords">
@@ -106,6 +123,11 @@
 						<reference key="object" ref="993928743"/>
 						<reference key="parent" ref="0"/>
 					</object>
+					<object class="IBObjectRecord">
+						<int key="objectID">12</int>
+						<reference key="object" ref="61197634"/>
+						<reference key="parent" ref="0"/>
+					</object>
 				</array>
 			</object>
 			<dictionary class="NSMutableDictionary" key="flattenedProperties">
@@ -113,19 +135,50 @@
 				<string key="-1.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="-2.CustomClassName">UIResponder</string>
 				<string key="-2.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
+				<string key="12.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="8.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 			</dictionary>
 			<dictionary class="NSMutableDictionary" key="unlocalizedProperties"/>
 			<nil key="activeLocalization"/>
 			<dictionary class="NSMutableDictionary" key="localizations"/>
 			<nil key="sourceID"/>
-			<int key="maxID">11</int>
+			<int key="maxID">14</int>
 		</object>
 		<object class="IBClassDescriber" key="IBDocument.Classes">
 			<array class="NSMutableArray" key="referencedPartialClassDescriptions">
 				<object class="IBPartialClassDescription">
 					<string key="className">WSDeviceChooserController</string>
 					<string key="superclassName">UITableViewController</string>
+					<dictionary class="NSMutableDictionary" key="actions">
+						<string key="cancelButtonPressed:">id</string>
+						<string key="currentButtonPressed:">id</string>
+						<string key="tappedBehindView:">id</string>
+					</dictionary>
+					<dictionary class="NSMutableDictionary" key="actionInfosByName">
+						<object class="IBActionInfo" key="cancelButtonPressed:">
+							<string key="name">cancelButtonPressed:</string>
+							<string key="candidateClassName">id</string>
+						</object>
+						<object class="IBActionInfo" key="currentButtonPressed:">
+							<string key="name">currentButtonPressed:</string>
+							<string key="candidateClassName">id</string>
+						</object>
+						<object class="IBActionInfo" key="tappedBehindView:">
+							<string key="name">tappedBehindView:</string>
+							<string key="candidateClassName">id</string>
+						</object>
+					</dictionary>
+					<object class="NSMutableDictionary" key="outlets">
+						<string key="NS.key.0">tapBehindViewRecognizer</string>
+						<string key="NS.object.0">UITapGestureRecognizer</string>
+					</object>
+					<object class="NSMutableDictionary" key="toOneOutletInfosByName">
+						<string key="NS.key.0">tapBehindViewRecognizer</string>
+						<object class="IBToOneOutletInfo" key="NS.object.0">
+							<string key="name">tapBehindViewRecognizer</string>
+							<string key="candidateClassName">UITapGestureRecognizer</string>
+						</object>
+					</object>
 					<object class="IBClassDescriptionSource" key="sourceIdentifier">
 						<string key="majorKey">IBProjectSource</string>
 						<string key="minorKey">./Classes/WSDeviceChooserController.h</string>
@@ -135,8 +188,12 @@
 		</object>
 		<int key="IBDocument.localizationMode">0</int>
 		<string key="IBDocument.TargetRuntimeIdentifier">IBIPadFramework</string>
+		<object class="NSMutableDictionary" key="IBDocument.PluginDeclaredDependencyDefaults">
+			<string key="NS.key.0">com.apple.InterfaceBuilder.CocoaTouchPlugin.iPhoneOS</string>
+			<real value="1296" key="NS.object.0"/>
+		</object>
 		<bool key="IBDocument.PluginDeclaredDependenciesTrackSystemTargetVersion">YES</bool>
 		<int key="IBDocument.defaultPropertyAccessControl">3</int>
-		<string key="IBCocoaTouchPluginVersion">933</string>
+		<string key="IBCocoaTouchPluginVersion">1181</string>
 	</data>
 </archive>

wsabi2/WSDeviceSetupController.h
@@ -47,6 +47,7 @@ typedef enum {
 -(IBAction)checkAgainButtonPressed:(id)sender;
 -(IBAction)editAddressButtonPressed:(id)sender;
 -(IBAction)changeCaptureTypeButtonPressed:(id)sender;
+-(IBAction)tappedBehindView:(id)sender;
 
 -(void) dismissKeyboard:(UITapGestureRecognizer*)recog;
 
@@ -78,5 +79,7 @@ typedef enum {
 
 //Table view stuff
 @property (nonatomic, strong) IBOutlet UITableView *tableView;
+
+@property (nonatomic, strong) IBOutlet UITapGestureRecognizer *tapBehindViewRecognizer;
 
 @end

wsabi2/WSDeviceSetupController.xib
@@ -16,6 +16,7 @@
 			<string>IBUILabel</string>
 			<string>IBUIImageView</string>
 			<string>IBUIActivityIndicatorView</string>
+			<string>IBUITapGestureRecognizer</string>
 			<string>IBUITableView</string>
 			<string>IBUIButton</string>
 		</array>
@@ -348,8 +349,10 @@
 					<reference key="NSCustomColorSpace" ref="250230817"/>
 				</object>
 				<bool key="IBUIClearsContextBeforeDrawing">NO</bool>
+				<array key="IBUIGestureRecognizers" id="0"/>
 				<string key="targetRuntimeIdentifier">IBIPadFramework</string>
 			</object>
+			<object class="IBUITapGestureRecognizer" id="881394836"/>
 		</array>
 		<object class="IBObjectContainer" key="IBDocument.Objects">
 			<array class="NSMutableArray" key="connectionRecords">
@@ -459,6 +462,14 @@
 				</object>
 				<object class="IBConnectionRecord">
 					<object class="IBCocoaTouchOutletConnection" key="connection">
+						<string key="label">tapBehindViewRecognizer</string>
+						<reference key="source" ref="841351856"/>
+						<reference key="destination" ref="881394836"/>
+					</object>
+					<int key="connectionID">41</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBCocoaTouchOutletConnection" key="connection">
 						<string key="label">dataSource</string>
 						<reference key="source" ref="151721332"/>
 						<reference key="destination" ref="841351856"/>
@@ -509,12 +520,20 @@
 					</object>
 					<int key="connectionID">32</int>
 				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBCocoaTouchEventConnection" key="connection">
+						<string key="label">tappedBehindView:</string>
+						<reference key="source" ref="881394836"/>
+						<reference key="destination" ref="841351856"/>
+					</object>
+					<int key="connectionID">42</int>
+				</object>
 			</array>
 			<object class="IBMutableOrderedSet" key="objectRecords">
 				<array key="orderedObjects">
 					<object class="IBObjectRecord">
 						<int key="objectID">0</int>
-						<array key="object" id="0"/>
+						<reference key="object" ref="0"/>
 						<reference key="children" ref="1000"/>
 						<nil key="parent"/>
 					</object>
@@ -628,6 +647,11 @@
 						<reference key="object" ref="1040492050"/>
 						<reference key="parent" ref="122056587"/>
 					</object>
+					<object class="IBObjectRecord">
+						<int key="objectID">40</int>
+						<reference key="object" ref="881394836"/>
+						<reference key="parent" ref="0"/>
+					</object>
 				</array>
 			</object>
 			<dictionary class="NSMutableDictionary" key="flattenedProperties">
@@ -647,6 +671,7 @@
 				<string key="31.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="36.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="4.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
+				<string key="40.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="5.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<string key="7.IBPluginDependency">com.apple.InterfaceBuilder.IBCocoaTouchPlugin</string>
 				<real value="2" key="7.IBUIButtonInspectorSelectedStateConfigurationMetadataKey"/>
@@ -659,7 +684,7 @@
 			<nil key="activeLocalization"/>
 			<dictionary class="NSMutableDictionary" key="localizations"/>
 			<nil key="sourceID"/>
-			<int key="maxID">37</int>
+			<int key="maxID">42</int>
 		</object>
 		<object class="IBClassDescriber" key="IBDocument.Classes">
 			<array class="NSMutableArray" key="referencedPartialClassDescriptions">
@@ -672,6 +697,7 @@
 						<string key="cycleButtonPressed:">id</string>
 						<string key="doneButtonPressed:">id</string>
 						<string key="editAddressButtonPressed:">id</string>
+						<string key="tappedBehindView:">id</string>
 					</dictionary>
 					<dictionary class="NSMutableDictionary" key="actionInfosByName">
 						<object class="IBActionInfo" key="changeCaptureTypeButtonPressed:">
@@ -694,6 +720,10 @@
 							<string key="name">editAddressButtonPressed:</string>
 							<string key="candidateClassName">id</string>
 						</object>
+						<object class="IBActionInfo" key="tappedBehindView:">
+							<string key="name">tappedBehindView:</string>
+							<string key="candidateClassName">id</string>
+						</object>
 					</dictionary>
 					<dictionary class="NSMutableDictionary" key="outlets">
 						<string key="changeCaptureTypeButton">UIButton</string>
@@ -707,6 +737,7 @@
 						<string key="statusContainerBackgroundView">UIImageView</string>
 						<string key="statusTextButton">UIButton</string>
 						<string key="tableView">UITableView</string>
+						<string key="tapBehindViewRecognizer">UITapGestureRecognizer</string>
 						<string key="warningContainer">UIView</string>
 					</dictionary>
 					<dictionary class="NSMutableDictionary" key="toOneOutletInfosByName">
@@ -753,6 +784,10 @@
 						<object class="IBToOneOutletInfo" key="tableView">
 							<string key="name">tableView</string>
 							<string key="candidateClassName">UITableView</string>
+						</object>
+						<object class="IBToOneOutletInfo" key="tapBehindViewRecognizer">
+							<string key="name">tapBehindViewRecognizer</string>
+							<string key="candidateClassName">UITapGestureRecognizer</string>
 						</object>
 						<object class="IBToOneOutletInfo" key="warningContainer">
 							<string key="name">warningContainer</string>

commit cbe2f6a62364a5d837a581e1a3426feeab575cb6

Fix #68 -- fix bug introduced in 8cae815f5d04e45bda300ada505ae8e62b05e142.
Perform the `pointInside` test on the parent `navigationController` rather than the content view, so that taps on the `titleView` cannot be mistaken as "outside" the view.

wsabi2/WSDeviceChooserController.m
@@ -156,7 +156,7 @@
         CGPoint location = [recognizer locationInView:nil];
         
         // Check if tap was within view
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+        if (![self.navigationController.view pointInside:[self.navigationController.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
             [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
             
             //post a notification to hide the device chooser and return to the previous state

wsabi2/WSDeviceSetupController.m
@@ -184,7 +184,7 @@
         CGPoint location = [recognizer locationInView:nil];
         
         // Check if tap was within view
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+        if (![self.navigationController.view pointInside:[self.navigationController.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
             [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
             [self dismissModalViewControllerAnimated:YES];
         }

wsabi2/WSModalityChooserController.m
@@ -97,7 +97,7 @@
         CGPoint location = [recognizer locationInView:nil];
         
         // Check if tap was within view
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+        if (![self.navigationController.view pointInside:[self.navigationController.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
             [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
             
             //post a notification to hide the device chooser and return to the previous state

wsabi2/WSSubmodalityChooserController.m
@@ -129,7 +129,7 @@
         CGPoint location = [recognizer locationInView:nil];
         
         // Check if tap was within view
-        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
+        if (![self.navigationController.view pointInside:[self.navigationController.view convertPoint:location fromView:self.view.window] withEvent:nil]) {
             [[[self view] window] removeGestureRecognizer:[self tapBehindViewRecognizer]];
             [self dismissModalViewControllerAnimated:YES];
         }