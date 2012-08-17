NIST-BWS/wsabi2

commit 50ae4d634cf3cef5f83d982f18945f699f7ff2dd

Orientation of a popover is always portrait (#89).
Introduces an issue where popover produced from modality 6 or 7 while in landscape is not redisplayed from the correct rect when rotated into portrait.

wsabi2/WSPersonTableViewCell.m
@@ -758,7 +758,7 @@
         //give the capture controller a reference to the correct item
         self.captureController.item = targetItem;
         
-        UIPopoverArrowDirection direction = UIInterfaceOrientationIsPortrait(self.captureController.interfaceOrientation) ? (UIPopoverArrowDirectionUp | UIPopoverArrowDirectionDown) :
+        UIPopoverArrowDirection direction = UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]) ? (UIPopoverArrowDirectionUp | UIPopoverArrowDirectionDown) :
         UIPopoverArrowDirectionAny;
         
         //make sure we have the right content size (may be reset elsewhere)

commit d76940ba46ed67387f0d7fe55e367fcc0c276a6e

Fix #89 and issue introduced in 50ae4d634cf3cef5f83d982f18945f699f7ff2dd.

wsabi2/WSPersonTableViewCell.h
@@ -42,8 +42,6 @@
     UIColor *normalBGColor;
     UIColor *selectedBGColor;
     
-    int selectedIndex;
-    
     UIPopoverController *capturePopover;
     UIPopoverController *biographicalPopover;
 }
@@ -83,6 +81,7 @@
 @property (nonatomic, strong) IBOutlet UIButton *duplicateRowButton;
 
 @property (nonatomic, strong) IBOutlet WSCaptureController *captureController;
+@property (nonatomic, readonly, assign) int selectedIndex;
 
 @property (nonatomic, strong) IBOutlet UIView *customSelectedBackgroundView;
 @property (nonatomic, strong) IBOutlet UIImageView *shadowUpView;

wsabi2/WSPersonTableViewCell.m
@@ -16,6 +16,7 @@
 @synthesize deletePersonOverlayViewCancelButton;
 @synthesize deletePersonOverlayViewDeleteButton;
 @synthesize captureController;
+@synthesize selectedIndex;
 @synthesize person;
 @synthesize itemGridView;
 @synthesize biographicalDataButton, biographicalDataInactiveLabel, timestampLabel, timestampInactiveLabel;
@@ -702,7 +703,7 @@
 
     cell.item = [orderedItems objectAtIndex:index];
     cell.active = self.selected;
-    cell.selected = (index == selectedIndex);
+    cell.selected = (index == self.selectedIndex);
     //cell.tempLabel.text = [NSString stringWithFormat:@"Grid Index %d\nInternal Index %d",index, [cell.item.index intValue]];
     cell.tag = GRID_CELL_OFFSET + index;
     return cell;
@@ -721,6 +722,10 @@
 -(void) showCapturePopoverAtIndex:(int) index
 {
     NSLog(@"Asking to show popover for item at index %d",index);
+    
+    // Force a redraw of the popover so that orientation is not reused
+    if ([capturePopover isPopoverVisible])
+        [capturePopover dismissPopoverAnimated:NO];
     
     WSItemGridCell *activeCell = (WSItemGridCell*)[self.itemGridView cellForItemAtIndex:index];
            

wsabi2/WSViewController.m
@@ -121,10 +121,12 @@
     return YES;
 }
 
--(void) willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
-    //dismiss any popover controller that's visible.
-    [self.popoverController dismissPopoverAnimated:YES];
+    // Redisplay capture controller if it was visible
+    WSPersonTableViewCell *cell = (WSPersonTableViewCell *)[[self tableView] cellForRowAtIndexPath:[[self tableView] indexPathForSelectedRow]];
+    if ([cell selectedIndex] != -1)
+        [cell showCapturePopoverAtIndex:[cell selectedIndex]];
 }
 
 #pragma mark - Notification action methods