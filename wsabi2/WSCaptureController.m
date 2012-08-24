NIST-BWS/wsabi2

commit 5268a1335ef7a782c703e13c8c288b13546851b0

* Fixes #101. * Remove dependency on UIView+FlipTransition, making the flip from front to flip of the CaptureController more fluid, more directional (right to left to right), and with no framing issues.

wsabi2/WSCaptureController.m
@@ -125,11 +125,9 @@
     
     self.backNavBarTitleItem.title = self.item.submodality;
     self.annotationNotesTableView.alwaysBounceVertical = NO;
-    
-    //if the back view is showing, flip it with a 0 duration.
-    if (!backContainer.hidden) {
-        [self showFlipSideAnimated:NO];
-    }
+
+    [[self backContainer] setHidden:NO];
+    [[self frontContainer] setHidden:NO];
     
     [self updateAnnotationLabel];
 }
@@ -267,7 +265,13 @@
 
 -(void)showFlipSideAnimated:(BOOL)animated
 {
-    [UIView flipTransitionFromView:self.frontContainer toView:self.backContainer duration:(animated == YES ? kFlipAnimationDuration : 0) completion:nil];
+    [UIView transitionFromView:[self frontContainer]
+                        toView:[self backContainer]
+                      duration:(animated ? kFlipAnimationDuration : 0)
+                       options:UIViewAnimationOptionTransitionFlipFromRight
+                    completion:nil
+     ];
+    
     annotating = YES;
 }
 
@@ -286,18 +290,26 @@
         self.itemDataView.backgroundColor = [UIColor darkGrayColor];
     }
     
-    [UIView flipTransitionFromView:self.backContainer toView:self.frontContainer duration:(animated == YES ? kFlipAnimationDuration : 0)
-                        completion:^(BOOL completed) {
-                            if (self.item.data) {
-                                self.itemDataView.alpha = 0;
-                                self.itemDataView.image = dataImage;
-                                [UIView animateWithDuration:0.1 animations:^{
-                                    self.itemDataView.alpha = 1.0;
-                                    self.itemDataView.backgroundColor = [UIColor whiteColor];
-                                }];
-                                
-                            }
-                        }];
+    void (^completion)(BOOL completed) = ^(BOOL completed) {
+        if (self.item.data) {
+            self.itemDataView.alpha = 0;
+            self.itemDataView.image = dataImage;
+            [UIView animateWithDuration:(animated ? 0.1 : 0)
+                             animations:^{
+                                 self.itemDataView.alpha = 1.0;
+                                 self.itemDataView.backgroundColor = [UIColor whiteColor];
+                             }
+             ];
+        }
+    };
+    
+    [UIView transitionFromView:[self backContainer]
+                        toView:[self frontContainer]
+                      duration:(animated ? kFlipAnimationDuration : 0)
+                       options:UIViewAnimationOptionTransitionFlipFromLeft
+                    completion:completion
+     ];
+
     //save the context
     [(WSAppDelegate*)[[UIApplication sharedApplication] delegate] saveContext];
     

wsabi2.xcodeproj/project.pbxproj
@@ -154,7 +154,6 @@
 		EA65DF7E1546D923006C826E /* UIPreferencesDeleteButtonPressed@2x.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DF7A1546D923006C826E /* UIPreferencesDeleteButtonPressed@2x.png */; };
 		EA65DF811546DBCC006C826E /* kb-extended-candidates-segmented-control-button-selected@2x.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DF7F1546DBCC006C826E /* kb-extended-candidates-segmented-control-button-selected@2x.png */; };
 		EA65DF821546DBCC006C826E /* kb-extended-candidates-segmented-control-button@2x.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DF801546DBCC006C826E /* kb-extended-candidates-segmented-control-button@2x.png */; };
-		EA65DFC3154739BD006C826E /* UIView+FlipTransition.m in Sources */ = {isa = PBXBuildFile; fileRef = EA65DFC2154739BD006C826E /* UIView+FlipTransition.m */; };
 		EA65DFD215483769006C826E /* symbol-notok.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DFCE15483769006C826E /* symbol-notok.png */; };
 		EA65DFD315483769006C826E /* symbol-notok@2x.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DFCF15483769006C826E /* symbol-notok@2x.png */; };
 		EA65DFD415483769006C826E /* symbol-ok.png in Resources */ = {isa = PBXBuildFile; fileRef = EA65DFD015483769006C826E /* symbol-ok.png */; };
@@ -531,8 +530,6 @@
 		EA65DF7A1546D923006C826E /* UIPreferencesDeleteButtonPressed@2x.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "UIPreferencesDeleteButtonPressed@2x.png"; sourceTree = "<group>"; };
 		EA65DF7F1546DBCC006C826E /* kb-extended-candidates-segmented-control-button-selected@2x.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "kb-extended-candidates-segmented-control-button-selected@2x.png"; sourceTree = "<group>"; };
 		EA65DF801546DBCC006C826E /* kb-extended-candidates-segmented-control-button@2x.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "kb-extended-candidates-segmented-control-button@2x.png"; sourceTree = "<group>"; };
-		EA65DFC1154739BD006C826E /* UIView+FlipTransition.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "UIView+FlipTransition.h"; sourceTree = "<group>"; };
-		EA65DFC2154739BD006C826E /* UIView+FlipTransition.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = "UIView+FlipTransition.m"; sourceTree = "<group>"; };
 		EA65DFC4154822D9006C826E /* wsabi2-4.25.2012.0817.xcdatamodel */ = {isa = PBXFileReference; lastKnownFileType = wrapper.xcdatamodel; path = "wsabi2-4.25.2012.0817.xcdatamodel"; sourceTree = "<group>"; };
 		EA65DFCE15483769006C826E /* symbol-notok.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "symbol-notok.png"; sourceTree = "<group>"; };
 		EA65DFCF15483769006C826E /* symbol-notok@2x.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; path = "symbol-notok@2x.png"; sourceTree = "<group>"; };
@@ -994,8 +991,6 @@
 				EA0DB8AD1536162000693D86 /* UIImage+Resize.m */,
 				EA0DB8AE1536162000693D86 /* UIImage+RoundedCorner.h */,
 				EA0DB8AF1536162000693D86 /* UIImage+RoundedCorner.m */,
-				EA65DFC1154739BD006C826E /* UIView+FlipTransition.h */,
-				EA65DFC2154739BD006C826E /* UIView+FlipTransition.m */,
 			);
 			name = Categories;
 			sourceTree = "<group>";
@@ -1641,7 +1636,6 @@
 				EA28CF1F153C4E7900A2F558 /* GMGridViewLayoutStrategies.m in Sources */,
 				EA28CF21153C4E7900A2F558 /* UIGestureRecognizer+GMGridViewAdditions.m in Sources */,
 				EA28CF23153C4E7900A2F558 /* UIView+GMGridViewAdditions.m in Sources */,
-				EA65DFC3154739BD006C826E /* UIView+FlipTransition.m in Sources */,
 				EA045286154EBA63007577B6 /* NSObject+GCDBlocks.m in Sources */,
 				EA8F3FE015613FDA00AC9A5A /* WSPopoverBackgroundView.m in Sources */,
 				EA8AA28B156BC391007D213A /* BlockActionSheet.m in Sources */,

wsabi2/UIView+FlipTransition.h
@@ -1,14 +1,0 @@
-//
-//  UIView+FlipTransition.h
-//  wsabi2
-//
-//  Created by Matt Aronoff on 4/24/12.
- 
-//
-
-#import <UIKit/UIKit.h>
-
-@interface UIView (FlipTransition)
-
-+ (void)flipTransitionFromView:(UIView *)firstView toView:(UIView *)secondView duration:(float)aDuration completion:(void (^)(BOOL finished))completion;
-@end

wsabi2/UIView+FlipTransition.m
@@ -1,113 +1,0 @@
-//
-//  UIView+FlipTransition.m
-//  wsabi2
-//
-//  Created by Matt Aronoff on 4/24/12.
- 
-//
-
-#import "UIView+FlipTransition.h"
-#import "constants.h"
-
-@implementation UIView (FlipTransition)
-
-//NOTE: Based on https://github.com/sgabello/UIView-FlipTransition
-+ (void)flipTransitionFromView:(UIView *)firstView toView:(UIView *)secondView duration:(float)aDuration completion:(void (^)(BOOL finished))completion
-{
-	firstView.layer.doubleSided = NO;
-	secondView.layer.doubleSided = NO;
-    
-	firstView.layer.zPosition = firstView.layer.bounds.size.width / 2;
-	secondView.layer.zPosition = secondView.layer.bounds.size.width / 2;
-    
-	CATransform3D transform = CATransform3DIdentity;
-    transform.m34 = -1.0f/500.0f;
-    
-	CGAffineTransform translation = CGAffineTransformMakeTranslation(secondView.layer.position.x - firstView.layer.position.x, secondView.layer.position.y - firstView.layer.position.y);
-    
-	CGAffineTransform scaling = CGAffineTransformMakeScale(secondView.bounds.size.width / firstView.bounds.size.width, secondView.bounds.size.height / firstView.bounds.size.height);
-    
-	CATransform3D rotation = CATransform3DRotate(transform, -0.999 * M_PI, 0.0f, 1.0f, 0.0f);
-    
-	CATransform3D firstViewTransform = CATransform3DConcat(rotation, CATransform3DMakeAffineTransform(CGAffineTransformConcat(scaling, translation)));
-    
-	CATransform3D secondViewTransform = CATransform3DConcat(CATransform3DInvert(rotation), CATransform3DMakeAffineTransform(CGAffineTransformConcat(CGAffineTransformInvert(scaling), CGAffineTransformInvert(translation))));
-    
-	if (secondView.hidden)
-	{
-		secondView.layer.transform = secondViewTransform;
-	}
-    
-	firstView.hidden = NO;
-	secondView.hidden = NO;
-    
-	CATransform3D firstToTransform = firstViewTransform;
-    CATransform3D secondToTransform = CATransform3DIdentity;
-	BOOL firstViewWillHide = YES;
-    
-    if (CATransform3DIsIdentity(secondView.layer.transform))
-    {
-		firstToTransform = CATransform3DIdentity;
-        secondToTransform = secondViewTransform;
-		firstViewWillHide = NO;
-    }
-    
-    [UIView animateWithDuration:aDuration
-                     animations:^(void){
-                         firstView.layer.transform = firstToTransform;
-                         secondView.layer.transform = secondToTransform;
-                     }
-                     completion:^(BOOL finished) {
-						 firstView.hidden = firstViewWillHide;
-						 secondView.hidden = !firstView.hidden;
-						 if (completion)
-							 completion(finished);
-					 }];
-}
-
-////Based on code from http://stackoverflow.com/questions/2644797/darkening-uiview-while-flipping-over-using-uiviewanimationtransitionflipfromright
-//- (void)flipFromView:(UIView*)viewOne toView:(UIView*)viewTwo
-//{
-//    viewOne.hidden = YES;
-//    
-//    CATransform3D matrix = CATransform3DMakeRotation (M_PI / 2, 0.0, 1.0, 0.0);
-//    CATransform3D matrix2 = CATransform3DMakeRotation (-M_PI / 2 , 0.0, 1.0, 0.0);
-//    matrix = CATransform3DScale (matrix, 1.0, 0.975, 1.0);
-//    matrix.m34 = 1.0 / -500;
-//    
-//    matrix2 = CATransform3DScale (matrix2, 1.0, 0.975, 1.0);
-//    matrix2.m34 = 1.0 / -500;
-//    
-//    viewOne.layer.transform = matrix2;
-//    
-//    [UIView animateWithDuration:kFlipAnimationDuration
-//                          delay:0 options:UIViewAnimationCurveEaseIn
-//                     animations:^{ 
-//                         viewTwo.layer.transform = matrix;
-//                     }
-//                     completion:^(BOOL completed) {
-//                         viewOne.hidden = NO;
-//                         viewTwo.hidden = YES;
-//                         
-//                         CATransform3D matrix = CATransform3DMakeRotation (2 * M_PI, 0.0, 1.0, 0.0);
-//                         
-//                         matrix = CATransform3DScale (matrix, 1.0, 1.0, 1.0);
-//                         int indexOfTwo = [self.subviews indexOfObject:viewTwo];
-//                         int indexOfOne = [self.subviews indexOfObject:viewOne];
-//
-//                         [UIView animateWithDuration:kFlipAnimationDuration
-//                                               delay:0 options:UIViewAnimationCurveEaseOut
-//                                          animations:^{ 
-//                                              viewOne.layer.transform = matrix;
-//                                          }
-//                                          completion:^(BOOL completed) {
-//                                              [self exchangeSubviewAtIndex:indexOfTwo withSubviewAtIndex:indexOfOne];
-//                                          }
-//                          ];
-//                         
-//                     }
-//     ];
-//    
-//}
-
-@end

wsabi2/WSCaptureController.h
@@ -13,7 +13,6 @@
 #import "WSCaptureButton.h"
 #import "NBCLDeviceLinkManager.h"
 #import "constants.h"
-#import "UIView+FlipTransition.h"
 
 #import "BlockAlertView.h"
 #import "BlockActionSheet.h"

wsabi2/WSViewController.h
@@ -31,6 +31,7 @@
     BOOL shouldRestoreCapturePopover;
     
     WSCDPerson* personBeingEdited;
+    BOOL wasAnnotating;
 }
 
 -(void) presentSensorWalkthrough:(NSNotification*)notification;

wsabi2/WSViewController.m
@@ -121,12 +121,26 @@
     return YES;
 }
 
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    // Record if the flip side of the CaptureController is presently shown
+    WSPersonTableViewCell *cell = (WSPersonTableViewCell *)[[self tableView] cellForRowAtIndexPath:[[self tableView] indexPathForSelectedRow]];
+    wasAnnotating = ((cell != nil) && ([cell selectedIndex] != -1) && ([[cell captureController] isAnnotating] == YES));
+    
+    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+}
+
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    
     // Redisplay capture controller if it was visible
     WSPersonTableViewCell *cell = (WSPersonTableViewCell *)[[self tableView] cellForRowAtIndexPath:[[self tableView] indexPathForSelectedRow]];
-    if ([cell selectedIndex] != -1)
+    if ((cell != nil) && ([cell selectedIndex] != -1)) {
         [cell showCapturePopoverAtIndex:[cell selectedIndex]];
+        if (wasAnnotating)
+            [[cell captureController] showFlipSideAnimated:NO];
+    }
 }
 
 #pragma mark - Notification action methods

commit 7f9acd20daf57327c084f74c2318bb53bece1856

Correct regression in 5268a1335ef7a782c703e13c8c288b13546851b0 where if annotating and another modality was selected, the annotation screen for the new modality would still appear.

wsabi2/WSCaptureController.m
@@ -130,6 +130,8 @@
     [[self frontContainer] setHidden:NO];
     
     [self updateAnnotationLabel];
+    
+    [self showFrontSideAnimated:NO];
 }
 
 - (void)viewDidLoad