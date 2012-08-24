NIST-BWS/wsabi2

commit f579601a587b71da226e53a100ff9372768e9e2f

Improvements to row selection.
* Row height is no longer animated, so modalities don't overlap buttons for a split second
* Separator, new person button, and timestamp are not faded out/in -- too jarring

wsabi2/WSViewController.m
@@ -445,16 +445,19 @@
 
 - (void)tableView:(UITableView *)aTableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-   
     selectedIndex = indexPath;
     
-    [aTableView beginUpdates];
-    [aTableView endUpdates];
-       
+    [aTableView reloadData];
+
     //If this is a currently deselected row, scroll to it.
     if (selectedIndex.section != previousSelectedIndex.section || selectedIndex.row != previousSelectedIndex.row) {
         [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];
-    }
+        
+        // reloadData resets selection (begin/endUpdates doesn't, but animates height change
+        [[aTableView cellForRowAtIndexPath:indexPath] setSelected:YES animated:YES];
+    } else
+        [[aTableView cellForRowAtIndexPath:indexPath] setSelected:YES animated:NO];
+    
     previousSelectedIndex = selectedIndex;
 }
 

wsabi2/WSPersonTableViewCell.m
@@ -227,41 +227,47 @@
     
     [super setSelected:selected animated:animated];
     
+    void (^preSelectionAnimation)(void) = ^(void) {
+        self.separatorView.alpha = (selected ? 0.0 : 1.0);
+        self.duplicateRowButton.alpha = (selected ? 1.0 : 0.0);
+        self.timestampInactiveLabel.alpha = (selected ? 0.0 : 1.0);
+    };
+    
     void (^toggleSelection)(void) = ^(void) {
-        self.duplicateRowButton.highlighted = !selected;
-        self.biographicalDataButton.highlighted = !selected;
-        self.addButton.highlighted = !selected;
-        self.editButton.highlighted = !selected;
-        self.deleteButton.highlighted = !selected;
-        
         self.shadowUpView.alpha = (selected ? 1.0 : 0.0);
         self.shadowDownView.alpha = (selected ? 1.0 : 0.0);
         self.timestampLabel.alpha = (selected ? 1.0 : 0.0);
         self.biographicalDataButton.alpha = (selected ? 1.0 : 0.0);
-        self.timestampInactiveLabel.alpha = (selected ? 0.0 : 1.0);
         self.biographicalDataInactiveLabel.alpha = (selected ? 0.0 : 1.0);
-        self.duplicateRowButton.alpha = (selected ? 1.0 : 0.0);
         self.addButton.alpha = (selected ? 1.0 : 0.0);
         self.editButton.alpha = (selected ? 1.0 : 0.0);
         self.deleteButton.alpha = (selected ? 1.0 : 0.0);
         self.itemGridView.alpha = (selected ? 1.0 : 0.3);
         self.customSelectedBackgroundView.backgroundColor = (selected ? selectedBGColor : normalBGColor);
         self.deletePersonOverlayView.alpha = (selected ? 0.0 : 1.0);
-        self.separatorView.alpha = (selected ? 0.0 : 1.0);
             
         if (selected) {
             self.contentView.alpha = 1.0;
             self.deletePersonOverlayView.hidden = YES;
         }
+    };
     
+    void (^toggleSelectionCompletion)(BOOL finished) = ^(BOOL finished) {
         [self layoutGrid];
         self.itemGridView.userInteractionEnabled = selected;
     };
-
+    
+    
+    preSelectionAnimation();
     if (animated)
-        [UIView animateWithDuration:kTableViewContentsAnimationDuration animations:toggleSelection];
-    else
+        [UIView animateWithDuration:kTableViewContentsAnimationDuration
+                         animations:toggleSelection
+                         completion:toggleSelectionCompletion];
+    else {
         toggleSelection();
+        toggleSelectionCompletion(YES);
+    }
+
     
     if (selected) {
         //Set up sensor links for each item in this person's record.

commit c7535579560ad1ea560855203f1cbd6bedcb388d

Re-fix #89 after regression in f579601a587b71da226e53a100ff9372768e9e2f.

wsabi2/WSViewController.m
@@ -448,6 +448,8 @@
     selectedIndex = indexPath;
     
     [aTableView reloadData];
+    // reloadData causes the row to become deselected, but is necessary to adjust row height
+    [aTableView selectRowAtIndexPath:selectedIndex animated:NO scrollPosition:UITableViewScrollPositionNone];
 
     //If this is a currently deselected row, scroll to it.
     if (selectedIndex.section != previousSelectedIndex.section || selectedIndex.row != previousSelectedIndex.row) {