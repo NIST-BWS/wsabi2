bbc6f72b59e03f60c1ef14c39eae0a977466e2b8
Greg Fiumara <gregory.fiumara@nist.gov>
2012-11-15 12:50:58 -0500

    Log biological popover closing.

--- wsabi2/views/interfaceElements/sources/WSPersonTableViewCell.m
+++ wsabi2/views/interfaceElements/sources/WSPersonTableViewCell.m
@@ -428,6 +428,7 @@
 
     biographicalPopover.popoverBackgroundViewClass = [WSPopoverBackgroundView class];
     biographicalPopover.contentViewController = tempNav;
+    biographicalPopover.delegate = self;
      
     biographicalPopover.popoverContentSize = CGSizeMake(cap.view.bounds.size.width, cap.view.bounds.size.height + 36); //leave room for the nav bar
         
@@ -1058,6 +1059,7 @@
     //make sure nothing is selected.
     [self selectItem:nil];
 
+    [self logPopoverHidden];
 }
 
 @end
