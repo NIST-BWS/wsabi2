fc8930730b62f00d3c77a3ea45681be1c6f532ac
Greg Fiumara <gregory.fiumara@nist.gov>
2013-02-04 12:06:38 -0500

    Comment API endpoints.

--- wsabi2/NetworkCommunication/Headers/WSDeviceLink.h
+++ wsabi2/NetworkCommunication/Headers/WSDeviceLink.h
@@ -61,23 +61,122 @@
 // Operations/API Endpoints
 //
 
+/// @brief
+/// Open a new client-server session.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)registerClient:(NSURL *)deviceID;
+
+/// @brief
+/// Close a client-server session.
+/// @param sessionId
+/// Identity of the session to remove.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)unregisterClient:(NSString *)sessionId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Initialize the target biometric sensor.
+/// @param sessionId
+/// Identity of the session requesting initialization.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)initialize:(NSString *)sessionId deviceID:(NSURL *)deviceID;
 
+/// @brief
+/// Retrieve metadata about the target biometric sensor's current configuration.
+/// @param sessionId
+/// Identity of the session requesting the configuration.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)getConfiguration:(NSString *)sessionId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Set the target biometric sensor's configuration.
+/// @param sessionId
+/// Identity of the session setting the configuration.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)setConfiguration:(NSString *)sessionId withParameters:(NSDictionary *)params deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Retrieve metadata about the service.
+/// @details
+/// The metadata does not depend on session-specifc information or soverign
+/// control of the target biometric sensor.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)getServiceInfo:(NSURL *)deviceID;
+
+/// @brief
+/// Cancel the current sensor operation.
+/// @param sessionId
+/// Identity of the session requesting cancellation.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)cancel:(NSString *)sessionId deviceID:(NSURL *)deviceID;
 
+/// @brief
+/// Try to obtain the service lock.
+/// @param sessionId
+/// Identity of the session requesting the service lock.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)lock:(NSString *)sessionId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Release the service lock.
+/// @param sessionId
+/// Identity of the session requesting the service lock.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)unlock:(NSString *)sessionId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Forcibly obtain the lock away from a peer client.
+/// @param sessionId
+/// Identity of the session requesting the service lock.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)stealLock:(NSString *)sessionId deviceID:(NSURL *)deviceID;
 
+/// @brief
+/// Capture biometric data.
+/// @param sessionId
+/// Identity of the session capturing the biometric data.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)capture:(NSString *)sessionId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Get the metadata associated with a capture.
+/// @param captureId
+/// Identity of the captured data to query.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)getDownloadInfo:(NSString *)captureId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Download the captured biometric data.
+/// @param captureId
+/// Identity of the captured data to download.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)download:(NSString *)captureId deviceID:(NSURL *)deviceID;
+
+/// @brief
+/// Download a scaled representatio of the captured biometric data ("thrifty").
+/// @param captureId
+/// Identify ot the captured data to download.
+/// @param maxSize
+/// Content-type dependent indicator of maximum permitted download size.
+/// @param deviceID
+/// URL-representation of the backing managed WSCDDeviceDefinition object.
 - (void)download:(NSString *)captureId withMaxSize:(float)maxSize deviceID:(NSURL *)deviceID;
+
+//
+// Predetermined operation sequences
+//
 
 - (BOOL)beginConnectSequenceWithDeviceID:(NSURL *)deviceID;
 - (BOOL)beginConnectConfigureSequenceWithConfigurationParams:(NSMutableDictionary *)params deviceID:(NSURL *)deviceID;
