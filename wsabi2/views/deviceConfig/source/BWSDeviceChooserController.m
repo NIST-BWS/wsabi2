NIST-BWS/wsabi2

commit 73e6eba32bf4d09fb07b2061ebf63758ebde2969

[Fix #158] Use emoji to display green checkmark.

wsabi2/views/deviceConfig/source/BWSDeviceChooserController.m
@@ -7,6 +7,7 @@
 // about its quality, reliability, or any other characteristic.
 
 #import "BWSDDLog.h"
+#import "UIImage+NBCLExtras.h"
 
 #import "BWSDeviceChooserController.h"
 
@@ -336,8 +337,6 @@
     
     NSString *titleString = nil;
     NSString *subtitleString = nil;
-    UIImage *greenCheckmark = [UIImage imageNamed:@"checkmark-green"];
-    [cell setIndentationWidth:greenCheckmark.size.width + 2];
     [cell setIndentationLevel:0];
     [cell startLoggingBWSInterfaceEventType:kBWSInterfaceEventTypeTap];
     
@@ -357,7 +356,7 @@
                         // Cannot use isEquals because of the way the recentSensors array is built
                         if ([[[[self item] deviceConfig] name] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] name]] &&
                             [[[[self item] deviceConfig] uri] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] uri]]) {
-                            [[cell imageView] setImage:greenCheckmark];
+                            [[cell imageView] setImage:[UIImage imageWithString:@"✅" font:[UIFont systemFontOfSize:30]]];
                             [cell setIndentationLevel:0];
                         } else
                             [cell setIndentationLevel:1];
@@ -403,7 +402,7 @@
                         // Cannot use isEquals because of the way the recentSensors array is built
                         if ([[[[self item] deviceConfig] name] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] name]] &&
                             [[[[self item] deviceConfig] uri] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] uri]]) {
-                            [[cell imageView] setImage:greenCheckmark];
+                            [[cell imageView] setImage:[UIImage imageWithString:@"✅" font:[UIFont systemFontOfSize:30]]];
                             [cell setIndentationLevel:0];
                         } else
                             [cell setIndentationLevel:1];

wsabi2.xcodeproj/project.pbxproj
@@ -114,7 +114,6 @@
 		EA0DB8B41536162000693D86 /* UIImage+RoundedCorner.m in Sources */ = {isa = PBXBuildFile; fileRef = EA0DB8AF1536162000693D86 /* UIImage+RoundedCorner.m */; };
 		EA277AA614DC3807002484CF /* CoreLocation.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = EA277AA514DC3807002484CF /* CoreLocation.framework */; };
 		EA277AB314DC4092002484CF /* BWSModalityMap.m in Sources */ = {isa = PBXBuildFile; fileRef = EA277AB214DC4092002484CF /* BWSModalityMap.m */; };
-		EA277AC514DC72C9002484CF /* checkmark-green.png in Resources */ = {isa = PBXBuildFile; fileRef = EA277AC414DC72C9002484CF /* checkmark-green.png */; };
 		EA277AC814DC7B8E002484CF /* UIView+FirstResponder.m in Sources */ = {isa = PBXBuildFile; fileRef = EA277AC714DC7B8E002484CF /* UIView+FirstResponder.m */; };
 		EA277AD114E1ACFD002484CF /* BreadcrumbButton.png in Resources */ = {isa = PBXBuildFile; fileRef = EA277AD014E1ACFD002484CF /* BreadcrumbButton.png */; };
 		EA277B0414EABF48002484CF /* PurchaseButtonBlue.png in Resources */ = {isa = PBXBuildFile; fileRef = EA277B0214EABF48002484CF /* PurchaseButtonBlue.png */; };
@@ -384,7 +383,6 @@
 		EA277AA514DC3807002484CF /* CoreLocation.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = CoreLocation.framework; path = System/Library/Frameworks/CoreLocation.framework; sourceTree = SDKROOT; };
 		EA277AB114DC4092002484CF /* BWSModalityMap.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = BWSModalityMap.h; path = NetworkCommunication/Headers/BWSModalityMap.h; sourceTree = "<group>"; };
 		EA277AB214DC4092002484CF /* BWSModalityMap.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = BWSModalityMap.m; path = NetworkCommunication/Sources/BWSModalityMap.m; sourceTree = "<group>"; };
-		EA277AC414DC72C9002484CF /* checkmark-green.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = "checkmark-green.png"; path = "Images/checkmark-green.png"; sourceTree = "<group>"; };
 		EA277AC614DC7B8E002484CF /* UIView+FirstResponder.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = "UIView+FirstResponder.h"; path = "Categories/Headers/UIView+FirstResponder.h"; sourceTree = "<group>"; };
 		EA277AC714DC7B8E002484CF /* UIView+FirstResponder.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = "UIView+FirstResponder.m"; path = "Categories/Sources/UIView+FirstResponder.m"; sourceTree = "<group>"; };
 		EA277AD014E1ACFD002484CF /* BreadcrumbButton.png */ = {isa = PBXFileReference; lastKnownFileType = image.png; name = BreadcrumbButton.png; path = Images/BreadcrumbButton.png; sourceTree = "<group>"; };
@@ -929,7 +927,6 @@
 				EA939CEF153DDCA3005BDA47 /* cell-drop-shadow-down@2x.png */,
 				EA939CF0153DDCA3005BDA47 /* cell-drop-shadow-up.png */,
 				EA939CF1153DDCA3005BDA47 /* cell-drop-shadow-up@2x.png */,
-				EA277AC414DC72C9002484CF /* checkmark-green.png */,
 				EA939C71153C8ED3005BDA47 /* Delete.png */,
 				EA939C72153C8ED3005BDA47 /* Delete@2x.png */,
 				EA939C73153C8ED3005BDA47 /* DeleteRed.png */,
@@ -1182,7 +1179,6 @@
 				EA8A051214C877FE00199FF8 /* glossyButton-black-disabled.png in Resources */,
 				EA8A051314C877FE00199FF8 /* glossyButton-black-highlighted.png in Resources */,
 				EA8A051414C877FE00199FF8 /* glossyButton-black-normal.png in Resources */,
-				EA277AC514DC72C9002484CF /* checkmark-green.png in Resources */,
 				EA277AD114E1ACFD002484CF /* BreadcrumbButton.png in Resources */,
 				EA277B0414EABF48002484CF /* PurchaseButtonBlue.png in Resources */,
 				EA277B0514EABF48002484CF /* PurchaseButtonBluePressed.png in Resources */,

wsabi2/Categories/Headers/UIImage+NBCLExtras.h
@@ -13,5 +13,6 @@
 @interface UIImage (NBCLExtras)
 
 + (UIImage *)scaleImage:(UIImage *)image toSize:(CGSize)targetSize;
++ (UIImage *)imageWithString:(NSString *)string font:(UIFont *)font;
 
 @end

wsabi2/Categories/Sources/UIImage+NBCLExtras.m
@@ -47,4 +47,21 @@
     return scaledImage;
 }
 
++ (UIImage *)imageWithString:(NSString *)string font:(UIFont *)font
+{
+    CGSize frameSize = [string sizeWithFont:font];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frameSize.width, frameSize.height)];
+    [label setFont:font];
+    [label setOpaque:NO];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setText:string];
+    
+    UIGraphicsBeginImageContextWithOptions(frameSize, NO, 0.0);
+    [label.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *retVal = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return (retVal);
+}
+
 @end

commit 8f388b2dda3f0ef50bb5df1838eaa9aa30c98bbd

Fix non-checkmark cell indentation.  Reuse image.
Regression from 73e6eba32bf4d09fb07b2061ebf63758ebde2969.

wsabi2/views/deviceConfig/source/BWSDeviceChooserController.m
@@ -337,6 +337,8 @@
     
     NSString *titleString = nil;
     NSString *subtitleString = nil;
+    UIImage *greenCheckmark = [UIImage imageWithString:@"✅" font:[UIFont systemFontOfSize:30]];
+    [cell setIndentationWidth:greenCheckmark.size.width];
     [cell setIndentationLevel:0];
     [cell startLoggingBWSInterfaceEventType:kBWSInterfaceEventTypeTap];
     
@@ -356,7 +358,7 @@
                         // Cannot use isEquals because of the way the recentSensors array is built
                         if ([[[[self item] deviceConfig] name] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] name]] &&
                             [[[[self item] deviceConfig] uri] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] uri]]) {
-                            [[cell imageView] setImage:[UIImage imageWithString:@"✅" font:[UIFont systemFontOfSize:30]]];
+                            [[cell imageView] setImage:greenCheckmark];
                             [cell setIndentationLevel:0];
                         } else
                             [cell setIndentationLevel:1];
@@ -402,7 +404,7 @@
                         // Cannot use isEquals because of the way the recentSensors array is built
                         if ([[[[self item] deviceConfig] name] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] name]] &&
                             [[[[self item] deviceConfig] uri] isEqualToString:[[recentSensors objectAtIndex:indexPath.row] uri]]) {
-                            [[cell imageView] setImage:[UIImage imageWithString:@"✅" font:[UIFont systemFontOfSize:30]]];
+                            [[cell imageView] setImage:greenCheckmark];
                             [cell setIndentationLevel:0];
                         } else
                             [cell setIndentationLevel:1];